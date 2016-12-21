d8b231b1c5b5da5c2c26fddc7f50c1abdc44e5ba
shonihei <shonihei@gmail.com>
2016-12-20 23:20:12 -0500

    Modify stuff

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,33 @@ function openInNewTab(url) {
     var win = window.open(url, '_blank');
     win.focus();
 }
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+        if (this.hash !== "") {
+            // Make sure this.hash has a value before overriding default behavior
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+    $("#contact-btn").click(function() {
+        $('html,body').animate({
+            scrollTop: $("#contact").offset().top},
+            900);
+        });
+    })
