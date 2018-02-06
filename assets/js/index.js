7ae09288c0902c4b8c3d431a7cae7a3570e59703
shonihei <shonihei@gmail.com>
2018-02-05 21:36:50 -0500

    got the scrolling function working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,6 @@
 $(document).ready(function() {
-	var keyframes = createKeyframeAnimation();
-	var style = document.createElement('style');
-	style.type = 'text/css';
-	style.innerHTML = keyframes
-	document.getElementsByTagName('head')[0].appendChild(style);
-	
-	$('.chunk').on('animationend', function(e) {
-		if (e.originalEvent.animationName === "pop") {
-			$(this).addClass('pulsate');
-		}
-	});
+	$('#projects').fullpage({
+        css3: true,
+        scrollingSpeed: 700,
+    });
 });
-
-function createKeyframeAnimation () {
-	var x = y = 0;
-	var animation = '';
-	var inverseAnimation = '';
-	
-	for (var step = 0; step <= 100; step++) {
-		var easedStep = exponential(step / 100);
-		const xScale = x + (1 - x) * easedStep;
-		
-		animation += `${step}% {
-			transform: scaleX(${xScale});
-		}`;
-		const invXScale = 1 / xScale;
-		inverseAnimation += `${step}% {
-			transform: scaleX(${invXScale});
-		}`;
-	}
-	
-	return `
-	@keyframes activityAnimation {
-		${animation}
-	}
-	
-	@keyframes activityContentsAnimation {
-		${inverseAnimation}
-	}`;
-}
-
-function exponential(k) {        
-	if (k === 0) return 0;
-	if (k === 1) return 1;
-	if ((k *= 2) < 1) return 0.5 * Math.pow(1024, k - 1);
-	return 0.5 * (- Math.pow(2, - 10 * (k - 1)) + 2);
-}
