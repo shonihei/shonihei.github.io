b1a2ff00c48912f8c3f12db3b8d6ca1976ebda43
shonihei <shonihei@gmail.com>
2018-02-12 21:02:14 -0500

    got the easing working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('#projects').fullpage({
         css3: true,
-        scrollingSpeed: 700,
+        scrollingSpeed: 800,
+        easingcss3: 'cubic-bezier(0.75,-0.5,0,1.1)',
     });
 });
